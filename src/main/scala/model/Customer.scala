98865052824b14249bd34e0e957575fa9a0c3993
JoolsF <joolmf@gmail.com>
2016-05-04 22:07:06 +0100

    Customer class setup

--- src/main/scala/model/Customer.scala
+++ src/main/scala/model/Customer.scala
@@ -19,16 +19,34 @@ case class Customer( id: Option[Long],
                      lastName:  String,
                      birthday: Option[java.util.Date])
 
-class Customers(tag: Tag) extends Table[Customer](tag, "customers"){
+class Customers(tag: Tag) extends Table[Customer](tag, "customers") {
 
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def firstName = column[String]("first_name")
   def lastName = column[String]("last_name")
-  //def birthday = column[java.util.Date]("birthday", O.Nullable) // can't add O.Nullable here
+  def birthday = column[Option[java.util.Date]]("birthday") // can't add O.Nullable here, using option instead
 
   //http://stackoverflow.com/questions/25158212/scala-problems-with-slick-2-when-case-class-and-object-name-are-same
   def * = Customer.apply _
 
+  /**
+   * An implicit dateTypeMapper was added due to default date mapping limitations. Slick supports only java.sql._ dates
+   * out of the box, but java.util.Date type is used to define birthday property of Customer objects.
+   */
+  implicit val dateTypeMapper = MappedColumnType.base[java.util.Date, java.sql.Date] (
+    ud => new java.sql.Date(ud.getTime),
+    sd => new java.util.Date(sd.getTime)
+  )
+
+  /**
+   * findById is a Slick's Query Template - parametrized query. A template works like a function that takes some
+   * parameters and returns a Query for them except that template is more efficient, because it doesn't
+   * require complete query recompilation at each run.
+   */
+  //TODO fix
+//  val findById = for {
+//    id <- Parameters[Long]
+//    c <- this if c.id is id
+//  } yield c
 
 }
-
