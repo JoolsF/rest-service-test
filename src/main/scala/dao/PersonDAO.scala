fe75eeb6350a937c4f4758b655226a6f79788840
JoolsF <joolmf@gmail.com>
2016-05-23 22:14:47 +0100

    DAO basic setup complete

--- src/main/scala/dao/PersonDAO.scala
+++ src/main/scala/dao/PersonDAO.scala
@@ -2,25 +2,33 @@ package dao
 
 import slick.driver.MySQLDriver.api._
 import model.PersonTable
+import slick.jdbc.meta.MTable
+
 import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  *
  */
 class PersonDAO {
+  //Init database instance
   val db = Database.forConfig("db")
 
-//
-  val people = TableQuery[PersonTable]
-////
-  val action: DBIO[Unit] = people.schema.create
-////
-  val future = db.run(action)
-////
-  val result = Await.result(future, 5 seconds)
+   /* Slick allows us to define and compose queries in advance of running them against the database.
+    We start by defining a TableQuery object that represents a simple SELECT * style query
+    on our message table: */
+    val people = TableQuery[PersonTable]
 
-println("running")
+    // Create tables if do not exist http://stackoverflow.com/questions/30036964/check-table-existance-in-slick-3-0
+    // TODO - implement below in non-blocking way
+    val tables = Await.result(db.run(MTable.getTables), 1.seconds).toList
+    if(tables.isEmpty) {
+      val action: DBIO[Unit] = people.schema.create
+      val future = db.run(action)
+      val result = Await.result(future, 5 seconds)
+      println("created table")
+    }
 
 }
